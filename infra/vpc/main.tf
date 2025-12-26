ac7f6bc1d4b2ef7f31a062a34355eb14bda3e2eb
GhanshyamRamole <ghanshyamramole999@gmail.com>
2025-12-26 18:16:09 +0000

    final-main.tf of vpc resources

---
+++ infra/vpc/main.tf
@@ -0,0 +1,112 @@
+resource "aws_vpc" "stage" {
+  cidr_block           = var.vpc_cidr
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+
+  tags = {
+    Name = "stage-vpc"
+  }
+}
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.stage.id
+
+  tags = {
+    Name = "stage-igw"
+  }
+}
+
+resource "aws_subnet" "public" {
+  count             = length(var.public_subnets)
+  vpc_id            = aws_vpc.stage.id
+  cidr_block        = var.public_subnets[count.index]
+  availability_zone = var.azs[count.index]
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "public-subnet-${count.index + 1}"
+  }
+}
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.stage.id
+
+  tags = {
+    Name = "public-rt"
+  }
+}
+
+resource "aws_route" "public_internet_access" {
+  route_table_id         = aws_route_table.public.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.igw.id
+}
+
+resource "aws_route_table_association" "public_subnet_association" {
+  count          = length(var.public_subnets)
+  subnet_id      = aws_subnet.public[count.index].id
+  route_table_id = aws_route_table.public.id
+}
+
+
+resource "aws_subnet" "private" {
+  count             = length(var.private_subnets)
+  vpc_id            = aws_vpc.stage.id
+  cidr_block        = var.private_subnets[count.index]
+  availability_zone = var.azs[count.index]
+  map_public_ip_on_launch = false
+
+  tags = {
+    Name = "private-subnet-${count.index + 1}"
+  }
+}
+resource "aws_eip" "nat_eip" {
+  
+  tags = {
+    Name = "nat-eip"
+  }
+}
+
+resource "aws_nat_gateway" "nat" {
+  allocation_id = aws_eip.nat_eip.id
+  # Creating NAT in Public Subnet 1
+  subnet_id     = aws_subnet.public[0].id   
+
+  tags = {
+    Name = "nat-gateway"
+  }
+
+  depends_on = [aws_internet_gateway.igw]
+}
+resource "aws_route_table" "private" {
+  vpc_id = aws_vpc.stage.id
+
+  tags = {
+    Name = "private-rt"
+  }
+}
+
+resource "aws_route" "private_nat_gateway" {
+  route_table_id         = aws_route_table.private.id
+  destination_cidr_block = "0.0.0.0/0"
+  nat_gateway_id         = aws_nat_gateway.nat.id
+}
+
+resource "aws_route_table_association" "private_subnet_association" {
+  count          = length(var.private_subnets)
+  subnet_id      = aws_subnet.private[count.index].id
+  route_table_id = aws_route_table.private.id
+}
+
+
+resource "aws_subnet" "db" {
+  count             = length(var.db_subnets)
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = var.db_subnets[count.index]
+  availability_zone = var.azs[count.index]
+  map_public_ip_on_launch = false
+
+  tags = {
+    Name = "db-subnet-${count.index + 1}"
+  }
+}
