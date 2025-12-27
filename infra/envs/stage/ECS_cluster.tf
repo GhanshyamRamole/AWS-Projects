06d0b0360ac5ee17844fd52a1d9927fb61e201ea
GhanshyamRamole <ghanshyamramole999@gmail.com>
2025-12-27 08:29:39 +0000

    ecs cluster

---
+++ infra/envs/stage/ECS_cluster.tf
@@ -0,0 +1,16 @@
+                          ###############################
+                          #         ECS Cluster         #
+                          ###############################
+                                  
+resource "aws_ecs_cluster" "app_cluster" {
+  name = "app-cluster"
+
+  setting {
+    name  = "containerInsights"
+    value = "enabled"
+  }
+
+  tags = {
+    Name = "app-cluster"
+  }
+}
